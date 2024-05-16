6011550e7cf1d8b3646baa135942acebfaea03e5
Bernard Igiri <bernard.igiri@gmail.com>
2024-05-16 14:12:25 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Fish Shell Dev Tools
+
+A collection of Fish shell functions useful for software development on Linux
+
+## How to use
+
+`source dev.fish`
+
+# Installation
+
+```bash
+vim ~/.config/fish/config.fish
+source ~/path/to/dev.fish
+```
---
+++ dev.fish
@@ -0,0 +1,31 @@
+function spawn_environment_from_env
+    set -l env_file $argv[1]
+    if test -f $env_file
+        for line in (cat $env_file | string trim)
+            # Skip comments and empty lines
+            if string match -q "#*" $line
+                continue
+            end
+            if string match -q "" $line
+                continue
+            end
+
+            # Split line by the first '='
+            set -l key (string split "=" $line)[1]
+            set -l value (string split -m1 "=" $line)[2..-1]
+
+            # Join value back together in case it was split incorrectly
+            set -l value (string join "=" $value)
+
+            # Remove quotes if present
+            set value (string trim -c "'" $value)
+            set value (string trim -c '"' $value)
+
+            # Export the variable
+            set -x $key $value
+        end
+        fish
+    else
+        echo "File $env_file does not exist."
+    end
+end
